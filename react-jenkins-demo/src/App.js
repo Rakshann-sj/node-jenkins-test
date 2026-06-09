f82d00f1097a13253b1eeb04350960009c1aae06
Rakshan <rakshann16.sj@gmail.com>
2026-06-09 17:49:07 +0530

    test2

---
+++ react-jenkins-demo/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div style={{textAlign:'center', marginTop:'100px'}}>
+      <h1>Jenkins React Demo</h1>
+      <h2>CI Pipeline Working Successfully</h2>
+      <h3>Rakshann SJ</h3>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-jenkins-demo/src/app.js
@@ -0,0 +1,11 @@
+kfunction App() {
+  return (
+    <div style={{textAlign:'center', marginTop:'100px'}}>
+      <h1>Jenkins React Demo</h1>
+      <h2>CI Pipeline Working Successfully</h2>
+      <h3>Rakshann SJ</h3>
+    </div>
+  );
+}
+
+export default App;
